fff7289daf2d1d6df7fbac09b4f2ebd18c087a35
mguizar88 <62078270+mguizar88@users.noreply.github.com>
2020-08-06 01:56:16 -0500

    Update nosotros.js

--- src/pages/nosotros.js
+++ src/pages/nosotros.js
@@ -4,6 +4,9 @@ import Layout from '../components/Layout'
 import '../components/index.sass'
 
 import chichen from '../../static/img/chichen-itza.jpg'
+import travel from '../../static/img/travel-plans.svg'
+import prices from '../../static/img/prices.svg'
+import tour from '../../static/img/tour.svg'
 
 export default class Nosotros extends React.Component {
   render() {
@@ -29,16 +32,37 @@ export default class Nosotros extends React.Component {
           </h1>
         </div>
         
-        <div className="container">
-          <p>
-            CAPPTOUR , somos una tour operadora 100% mexicana especializada en la planeación, diseño y organización de tours y experiencias personalizadas, con el firme objetivo de asegurar a nuestros viajeros una experiencia única, placentera e inolvidable.
-          </p>
-          <p>
-            Nuestro servicio, está enfocado en proporcionar calidad y confiabilidad, a través de la atención de nuestro personal debidamente capacitado, garantizando viajes únicos, logrando superar las expectativas de nuestros viajeros. 
-          </p>
-          <p>
-            Brindando dentro de nuestros servicios, los mejores costos en hoteles, renta de autos, traslados; así como tours en todos los puntos de interés.
-          </p>
+        <div style={{maxWidth: '768px',}} className="container">
+          <div className="columns is-vcentered">
+            <div className="column is-half-desktop is-12-mobile">
+              <p className="is-size-4-desktop">
+                CAPPTOUR , somos una tour operadora 100% mexicana especializada en la planeación, diseño y organización de tours y experiencias personalizadas, con el firme objetivo de asegurar a nuestros viajeros una experiencia única, placentera e inolvidable.
+              </p>
+            </div>
+            <div className="column is-half-desktop is-12-mobile">
+              <img src={travel} alt="travel plans" />
+            </div>
+          </div>
+          <div className="columns is-vcentered">
+            <div className="column is-half-desktop is-12-mobile">
+              <p className="is-size-4-desktop">
+                Nuestro servicio, está enfocado en proporcionar calidad y confiabilidad, a través de la atención de nuestro personal debidamente capacitado, garantizando viajes únicos, logrando superar las expectativas de nuestros viajeros. 
+              </p>
+            </div>
+            <div className="column is-half-desktop is-12-mobile">
+              <img src={tour} alt="travel plans" />
+            </div>
+          </div>
+          <div className="columns is-vcentered">
+            <div className="column is-half-desktop is-12-mobile">
+              <p className="is-size-4-desktop">
+                Brindando dentro de nuestros servicios, los mejores costos en hoteles, renta de autos, traslados; así como tours en todos los puntos de interés.
+              </p>
+            </div>
+            <div className="column is-half-desktop is-12-mobile">
+              <img src={price} alt="travel plans" />
+            </div>
+          </div>
         </div>
         
       </Layout>
