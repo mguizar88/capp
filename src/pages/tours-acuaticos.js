5edd9bbb6e533a6dfa8592d70c871f809cce14c3
mguizar88 <62078270+mguizar88@users.noreply.github.com>
2020-08-10 15:12:04 -0500

    [skip ci]

--- src/pages/tours-acuaticos.js
+++ src/pages/tours-acuaticos.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Layout from '../../components/Layout'
+import Layout from '../components/Layout'
 import { Link, graphql } from 'gatsby'
 import Img from "gatsby-image"
 
